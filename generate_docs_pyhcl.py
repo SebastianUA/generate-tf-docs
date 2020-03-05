ee7baf082a935ae5dd83b39ab9934a8bc19635f2
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2020-03-05 11:01:10 +0200

    Add hcl2 supporting

--- generate_docs_pyhcl.py
+++ generate_docs_pyhcl.py
@@ -1,113 +1,137 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import time
 import os
 import os.path
-import json
 import hcl
+import hcl2
 from lib.color import Bgcolor
 from lib.parser import ColoredArgParser
 
 
-def get_parent_dir(path):
-    return path.strip().split('/')[-1]
-
-
-def put_header(m_dir, e_dir):
-    dir_name = get_parent_dir(m_dir)
-    file_out = 'OUTPUT_{}.md'.format(dir_name)
-    examples_file = os.path.join(e_dir, "main.tf")
-    headers = """# Work with AWS {0} via terraform
+class TFGenerator(object):
+    TF_HEADER = """# Work with AWS {0} via terraform
 
 A terraform module for making {0}.
 
 
 ## Usage
 ----------------------
-Import the module and retrieve with ```terraform get``` or ```terraform get --update```. Adding a module resource to your template, e.g. `main.tf`:
-""".format(dir_name.upper())
+Import the module and retrieve with ```terraform get``` or \
+```terraform get --update```. Adding a module resource to your template, e.g. \
+`main.tf`:
+"""
 
-    with open(file_out, 'w+') as f:
-        f.write(headers + "\n")
-        f.write("```\n")
-        f_main = open(examples_file)
-        f.writelines(f_main.readlines())
-        f.write("```\n\n")
-
-
-def generate_inputs(m_dir):
-    tf_file_variables = os.path.join(m_dir, 'variables.tf')
-    if not os.path.isfile(tf_file_variables):
-        print(Bgcolor.fail('File doesnt exist! Check PATH to module!'))
-        print(Bgcolor.fail("You're trying to use [{}]".format(dir)))
-        raise ValueError()
-
-    dir_name = get_parent_dir(m_dir)
-    file_out = 'OUTPUT_{}.md'.format(dir_name)
-    input_header = """## Module Input Variables
-----------------------"""
-
-    with open(file_out, 'a') as f:
-        f.write(input_header + "\n")
-
-    with open(tf_file_variables) as fp_in:
-        try:
-            obj = hcl.load(fp_in)
-            for variable in obj['variable']:
-                description = obj['variable'][variable]['description']
-                default = obj['variable'][variable]['default']
-                line = '- `{}` - {} (`default = {}`)\n'.format(variable, description, default)
-                with open(file_out, 'a') as f:
-                    f.write(line)
-        except ValueError as err:
-            print(Bgcolor.fail(err))
-
-
-def generate_outputs(m_dir):
-    tf_file_output = os.path.join(m_dir, 'outputs.tf')
-    dir_name = get_parent_dir(m_dir)
-    file_out = 'OUTPUT_{}.md'.format(dir_name)
-    if os.path.isfile(tf_file_output):
-        output_header = """
-        
-## Module Output Variables
-----------------------"""
-        with open(file_out, 'a') as f:
-            f.write(output_header + "\n")
-
-    with open(tf_file_output) as fp_out:
-        try:
-            obj = hcl.load(fp_out)
-            for output in obj['output']:
-                description = obj['output'][output]['description'] or '""'
-                line = '- `{}` - {}'.format(output, description)
-                with open(file_out, 'a') as f:
-                    f.write(line + '\n')
-        except ValueError as err:
-            print(Bgcolor.fail(err))
-
-
-def put_footer(m_dir):
-    dir_name = m_dir.strip().split('/')[-1]
-    file_out = 'OUTPUT_{}.md'.format(dir_name)
-    authors = """
+    INPUT_HEADER = "## Module Input Variables\n----------------------"
+    OUTPUT_HEADER = "\n## Module Output Variables\n----------------------"
+    AUTHORS = """
 
 ## Authors
-=======
 
-Created and maintained by [Vitaliy Natarov](https://github.com/SebastianUA)
-(vitaliy.natarov@yahoo.com).
+Created and maintained by [Vitaliy Natarov](https://github.com/SebastianUA). \
+An email: [vitaliy.natarov@yahoo.com](vitaliy.natarov@yahoo.com).
 
-License
-=======
+## License
 
-Apache 2 Licensed. See [LICENSE](https://github.com/SebastianUA/terraform/blob/master/LICENSE) for full details."""
+Apache 2 Licensed. See [LICENSE]\
+(https://github.com/SebastianUA/terraform/blob/master/LICENSE) for full details."""
 
-    with open(file_out, 'a') as f:
-        f.write(authors + "\n")
+    def __init__(self, m_dir, e_dir, hcl_version=1):
+        self.m_dir, self.e_dir = m_dir, e_dir
+        self.hcl_version = hcl_version
+        self.parent_dir = self.get_parent_dir(m_dir)
+        self.file_out = 'OUTPUT_{}.md'.format(self.parent_dir)
 
-    print('"{0}" file has been created: {1}'.format(file_out, os.getcwd()))
+    @classmethod
+    def get_parent_dir(cls, path):
+        return path.strip().split('/')[-1]
+
+    def put_header(self):
+        examples_file = os.path.join(self.e_dir, "main.tf")
+        headers = self.TF_HEADER.format(self.parent_dir.upper())
+
+        with open(self.file_out, 'w+') as f_out:
+            f_out.write(headers + "\n")
+            f_out.write("```\n")
+            f_main = open(examples_file)
+            f_out.writelines(f_main.readlines())
+            f_out.write("```\n\n")
+
+    def generate_inputs(self):
+        tf_file_variables = os.path.join(self.m_dir, 'variables.tf')
+        if not os.path.isfile(tf_file_variables):
+            print(Bgcolor.fail('File doesnt exist! Check PATH to module!'))
+            print(Bgcolor.fail("You're trying to use [{}]".format(self.m_dir)))
+            raise ValueError()
+
+        with open(self.file_out, 'a') as f_out:
+            f_out.write(self.INPUT_HEADER + "\n")
+
+        with open(tf_file_variables) as fp_in:
+            try:
+                if self.hcl_version == 1:
+                    obj = hcl.load(fp_in)
+                    for variable in obj['variable']:
+                        description = obj['variable'][variable]['description'] or '""'
+                        default = obj['variable'][variable]['default'] or '""'
+                        line = '- `{}` - {} (`default = {}`)\n'.format(variable,
+                                                                       description,
+                                                                       default)
+                        with open(self.file_out, 'a')  as f_out:
+                            f_out.write(line)
+                else:
+                    obj = hcl2.load(fp_in)
+                    for section in obj['variable']:
+                        # Dict contains only a single key, just take it
+                        variable = next(iter(section))
+                        description = section[variable]['description'][0] or '""'
+                        default = section[variable]['default'][0] or '""'
+                        line = '- `{}` - {} (`default = {}`)\n'.format(variable,
+                                                                       description,
+                                                                       default)
+                        with open(self.file_out, 'a') as f_out:
+                            f_out.write(line)
+            except ValueError as err:
+                print(Bgcolor.fail(err))
+
+    def generate_outputs(self):
+        tf_file_output = os.path.join(self.m_dir, 'outputs.tf')
+        if os.path.isfile(tf_file_output):
+            with open(self.file_out, 'a') as f_out:
+                f_out.write(self.OUTPUT_HEADER + "\n")
+
+        with open(tf_file_output) as fp_out:
+            try:
+                if self.hcl_version == 1:
+                    obj = hcl.load(fp_out)
+                    for output in obj['output']:
+                        description = obj['output'][output]['description'] or '""'
+                        line = '- `{}` - {}'.format(output, description)
+                        with open(self.file_out, 'a')  as f_out:
+                            f_out.write(line + '\n')
+                else:
+                    obj = hcl2.load(fp_out)
+                    for section in obj['output']:
+                        # Dict contains only a single key, just take it
+                        output = next(iter(section))
+                        description = section[output]['description'][0] or '""'
+                        line = '- `{}` - {}'.format(output, description)
+                        with open(self.file_out, 'a')  as f_out:
+                            f_out.write(line + '\n')
+            except ValueError as err:
+                print(Bgcolor.fail(err))
+
+    def put_footer(self):
+        with open(self.file_out, 'a')  as f_out:
+            f_out.write(self.AUTHORS + "\n")
+
+        print('"{0}" file has been created: {1}'.format(self.file_out, os.getcwd()))
+
+    def generate(self):
+        self.put_header()
+        self.generate_inputs()
+        self.generate_outputs()
+        self.put_footer()
 
 
 def main():
@@ -116,29 +140,28 @@ def main():
             return path
         raise ValueError("{} not a directory".format(path))
 
-    start__time = time.time()
     parser = ColoredArgParser(prog='python3 script_name.py -h',
                               usage='python3 script_name.py {ARGS}',
                               add_help=True,
                               prefix_chars='--/',
                               epilog='''created by Vitalii Natarov''')
     parser.add_argument('--version', action='version', version='v1.0.0')
-    parser.add_argument('-m', '--modules', dest='m_directory', help='Set the directory where module exists',
+    parser.add_argument('-m', '--modules', dest='m_directory',
+                        help='Set the directory where module exists',
                         metavar='DIR', type=dir_path, required=True)
-    parser.add_argument('-e', '--examples', dest='e_directory', help='Set the directory where example exists',
+    parser.add_argument('-e', '--examples', dest='e_directory',
+                        help='Set the directory where example exists',
                         metavar='DIR', type=dir_path, required=True)
+    parser.add_argument('--hcl-version', dest='hcl_version',
+                        help='Set version of hcl to use for parsing terraform',
+                        metavar='HCL VERSION', type=int, default=1, choices=[1, 2])
+    args = parser.parse_args()
 
-    results = parser.parse_args()
-    m_directory = results.m_directory
-    e_directory = results.e_directory
+    tf_generator = TFGenerator(args.m_directory,
+                               args.e_directory,
+                               args.hcl_version)
+    tf_generator.generate()
 
-    put_header(m_directory, e_directory)
-    generate_inputs(m_directory)
-    generate_outputs(m_directory)
-    put_footer(m_directory)
-
-    end__time = round(time.time() - start__time, 2)
-    print("--- %s seconds ---" % end__time)
     print(Bgcolor.green("=" * 60))
     print(Bgcolor.green("=" * 25 + " FINISHED " + "=" * 25))
     print(Bgcolor.green("=" * 60))
